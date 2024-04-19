d85c5014d1f42364b7a6b2607f96874ecff545d5
Mossa <cgmossa@gmail.com>
2024-04-19 14:36:56 +0200

    initial gillespie approach

--- src/rust/src/birth_death_discrete.rs
+++ src/rust/src/birth_death_discrete.rs
@@ -1,5 +1,6 @@
 use extendr_api::prelude::*;
-use itertools::Itertools;
+use itertools::{izip, Itertools};
+use rand::{rngs::SmallRng, Rng, SeedableRng};
 use readonly::make as readonly;
 
 use crate::linspace;
@@ -29,8 +30,10 @@ fn rate_to_probability(rate: f64) -> f64 {
     1. - (-rate).exp()
 }
 
+#[extendr]
 fn sim_bd_demo() {
-    simulate_birth_death(1.9875, 0.6540, 80.)
+    let result = simulate_birth_death(1.9875, 0.6540, 80., 400);
+    todo!()
 }
 
 #[derive(Debug)]
@@ -40,106 +43,136 @@ pub(crate) struct State {
     count: f64,
 }
 
-fn simulate_birth_death(birth: f64, death: f64, carrying_capacity: f64) {
+fn simulate_birth_death(birth: f64, death: f64, carrying_capacity: f64, reps: usize) -> Vec<State> {
     // birth  death
     // 1.9875 0.6540
     let birth_baseline = birth;
     let death_baseline = death;
     let adjustment = (birth_baseline - death_baseline) / (birth_baseline + death_baseline);
+    // carrying_capacity example is 80
     let k0 = carrying_capacity;
 
-    let reps = 20 * 5 * 2;
+    // let reps = 20 * 5 * 2; // 400
     let max_t_years = 25.;
     let u0 = linspace::range(1., 12., 0.5).collect_vec();
     let mut current_u = u0.repeat(reps);
     let mut current_t = [0.].repeat(reps);
 
     let mut results: Vec<State> = Vec::with_capacity(1000);
-    //   id_rep <- seq_along(current_u)
-    // add initial states
-    for rep in 0..reps {
-        for (&count, &time) in u0.iter().zip_eq(current_t.iter()) {
+    let mut current_rep = (0..reps * u0.len()).collect_vec();
+    // record initial states
+    for (&repetition, &count, &time) in izip!(current_rep.iter(), u0.iter(), current_t.iter()) {
+        results.push(State {
+            repetition: repetition + 1,
+            time,
+            count,
+        });
+    }
+
+    let mut rng = SmallRng::seed_from_u64(20240416);
+    // elapsed and extinct `rep` in a given timestep
+    let mut elapsed = Vec::with_capacity(current_rep.len());
+    // extinct is defined as u == 0 prior to max_t_years.
+    let mut extinct = Vec::with_capacity(current_rep.len());
+    // note: extinct and elapsed must not overlap.
+
+    loop {
+        assert!(!current_u.is_empty());
+        assert!(!current_t.is_empty());
+
+        // clear the indices buffers
+        elapsed.clear();
+        extinct.clear();
+
+        let current_n = current_u.len();
+
+        let next_unif = rng
+            // FIXME: investigate the consequence of this clone
+            .clone()
+            .sample_iter::<f64, _>(rand::distributions::Open01)
+            .take(current_n)
+            .collect_vec();
+
+        // does it not suffice that we are doing this at the end?
+        // assert_eq!(current_rep.len(), current_t.len());
+        // assert_eq!(current_t.len(), current_u.len());
+        // assert_eq!(current_u.len(), next_unif.len());
+
+        for (rep, u, next_unif, t) in izip!(
+            current_rep.iter(),
+            current_u.iter_mut(),
+            next_unif.into_iter(),
+            current_t.iter_mut()
+        ) {
+            let birth = birth_baseline * (1. - (adjustment * *u) / k0);
+            let death = death_baseline * (1. + (adjustment * *u) / k0);
+            assert!(birth > 0.);
+            assert!(death > 0.);
+            // ALTERNATIVE:
+            // delta_t <- rexp(n = current_n, rate = (birth + death) * current_u)
+            let delta_t = -next_unif.ln() / ((birth + death) * *u);
+            assert!(delta_t.is_finite());
+            *t += delta_t;
+
+            // let p_birth = rate_to_probability(birth);
+            // let p_death = rate_to_probability(death);
+            let delta_u = rng.gen_bool(birth / (birth + death));
+            // let delta_u = rng.sample(rand::distributions::Bernoulli::new(birth / (birth + death)))
+            let delta_u = if delta_u { 1. } else { 0. };
+            let delta_u = 2. * delta_u - 1.;
+            *u += delta_u;
+            // or is the population past simulation time?
+            if *t >= max_t_years {
+                elapsed.push(*rep);
+            } else {
+                // is the population extinct?
+                if *u == 0. {
+                    extinct.push(*rep);
+                }
+            }
+            // what if a group is elapsed and extinct?
+            // an extinct group is defined as extinct if it dies out prior to
+            // the end of the simulation
+        }
+
+        // record
+        for (&rep, &time, &count) in izip!(current_rep.iter(), current_t.iter(), current_u.iter()) {
             results.push(State {
                 repetition: rep + 1,
                 time,
                 count,
-            })
+            });
+        }
+
+        // retire elapsed trajectories
+
+        // record end time for extinct groups
+        for rep_extinct in extinct.iter() {
+            results.push(State {
+                repetition: rep_extinct + 1,
+                time: max_t_years,
+                count: 0.,
+            });
+        }
+
+        // remove the elapsed simulations from consideration
+        while let Some((id, rep)) = current_rep.iter().enumerate().next() {
+            if !extinct.contains(rep) && !elapsed.contains(rep) {
+                continue;
+            }
+            current_rep.remove(id);
+            current_t.remove(id);
+            current_u.remove(id);
+        }
+        
+        assert_eq!(current_rep.len(), current_t.len());
+        assert_eq!(current_t.len(), current_u.len());
+
+        // if there are no more to simulate, exit
+        if current_u.is_empty() {
+            break;
         }
     }
-    loop {
-
-        //   repeat {
-
-        //     stopifnot(all(!is.na(current_u),
-        //                   !is.na(current_t)))
-
-        //     current_n <- length(current_u)
-        //     next_unif <- runif(current_n)
-
-        //     stopifnot(
-        //       vapply(next_unif, \(x) !isTRUE(all.equal.numeric(x, 0)), FUN.VALUE = logical(1))
-        //     )
-        //     # stopifnot(current_u < k0)
-
-        //     birth <- birth_baseline * (1 - (birth_baseline - death_baseline) / (birth_baseline + death_baseline) * current_u / k0)
-        //     death <- death_baseline * (1 + (birth_baseline - death_baseline) / (birth_baseline + death_baseline) * current_u / k0)
-        //     stopifnot(all(birth > 0), all(death > 0))
-
-        //     delta_t <- -log(next_unif) / ((birth + death) * current_u)
-        //     # ALTERNATIVE:
-        //     # delta_t <- rexp(n = current_n, rate = (birth + death) * current_u)
-        //     stopifnot(all(!is.infinite(delta_t)))
-        //     next_t <- current_t + delta_t
-
-        //     # IDEA: use rexp instead you crazy person
-        //     # birth <- yearly_prob_to_weekly_prob(rate_to_probability(birth))
-        //     # death <- yearly_prob_to_weekly_prob(death)
-        //     birth <- rate_to_probability(birth)
-        //     death <- rate_to_probability(death)
-        //     delta_u <- rbinom(n = current_n, size = 1, prob = birth / (birth + death))
-        //     delta_u <- 2 * delta_u - 1
-        //     next_u <- current_u + delta_u
-
-        //     # shift
-        //     current_u <- next_u
-        //     current_t <- next_t
-
-        //     # record
-        //     results <- rbind(
-        //       results,
-        //       cbind(rep = id_rep, t = current_t, u = current_u)
-        //     )
-
-        //     # retire elapsed trajectories
-        //     # elapsed_t <- which(current_t >= max_t_weeks)
-        //     elapsed_t <- which(current_t >= max_t_years)
-        //     elapsed_u <- which(current_u == 0)
-        //     elapsed_reps <- unique(c(elapsed_t, elapsed_u))
-
-        //     if (length(elapsed_reps) > 0) {
-        //       # message(glue("{length(elapsed_u)}"))
-
-        //       #add `t_max` to the `elapsed_u` group
-        //       results <- rbind(
-        //         results, cbind(rep = id_rep[elapsed_u],
-        //                        # t = current_t[elapsed_u],
-        //                        t = rep.int(max_t_years, length(elapsed_u)),
-        //                        u = current_u[elapsed_u])
-        //       )
-
-        //       # remove the elapsed simulations from consideration
-        //       current_u <- current_u[-elapsed_reps]
-        //       current_t <- current_t[-elapsed_reps]
-        //       id_rep <- id_rep[-elapsed_reps]
-        //     }
-        //     stopifnot(length(current_u) == length(current_t),
-        //               length(current_t) == length(id_rep)
-        //     )
-        //     if (length(current_u) == 0) {
-        //       break
-        //     }
-        //   }
-    }
     //TODO: return something
-    
+    results
 }
