bf96c2d93839f1e7f79e04a2f82312a5c3c7852d
Mossa <cgmossa@gmail.com>
2024-04-25 13:02:36 +0200

    maybe this is a fix?

--- notebooks/020_separate_k.R
+++ notebooks/020_separate_k.R
@@ -1,4 +1,11 @@
 
+ode_params <- list(
+  beta_0 = 4,
+  mu_0 = 1,
+  K_beta = 120,
+  K_mu = 15,
+  alpha_mu = 0.1
+)
 
 birth <- function(N, beta_0, K_beta) {
   beta_0 / K_beta * pmax(0, K_beta - N)
@@ -9,27 +16,32 @@ death <- function(N, mu_0, K_mu, alpha_mu) {
 }
 
 carrying_capacity <- function(beta_0, K_beta, mu_0, K_mu, alpha_mu) {
-  (beta_0 * K_beta * K_mu + mu_0 * K_beta * K_mu) /
-    (beta_0 * K_mu + mu_0 * K_beta)
+  (
+    (K_beta * K_mu * (beta_0  + mu_0)) /
+      (beta_0 * K_mu + mu_0 * K_beta)
+  ) * (K_beta > K_mu) +
+    (
+      (K_beta * K_mu * (beta_0  - mu_0)) /
+        (beta_0 * K_mu - mu_0 * K_beta)
+    ) * (K_beta <= K_mu)
+
+  # (K_beta * K_mu * (beta_0  - mu_0 )) /
+  #   (beta_0 * K_mu - mu_0 * K_beta )
 }
 
-tibble(
-  N = seq.default(0, 25, by = 1),
-) %>%
+expand_grid(ode_params %>% as_tibble()) %>%
+  mutate(
+    carrying_capacity = carrying_capacity(beta_0, K_beta, mu_0, K_mu, alpha_mu)
+  ) %>%
   expand_grid(
-    beta_0 = 4,
-    mu_0 = 0.5,
-    K_beta = 14,
-    K_mu = 10,
-    alpha_mu = 0.1
-    # alpha_mu = mu_0 / K_mu #silly suggestion
+    N = seq.default(0, (2) * max(carrying_capacity), by = 1)
   ) %>%
   glimpse() %>%
   mutate(
     birth = birth(N, beta_0, K_beta),
     death = death(N, mu_0, K_mu, alpha_mu),
     growth = birth - death,
-    carrying_capacity = carrying_capacity(beta_0, K_beta, mu_0, K_mu, alpha_mu),
+    # carrying_capacity = carrying_capacity(beta_0, K_beta, mu_0, K_mu, alpha_mu),
     p_extinct = death / birth
   ) %>%
   pivot_longer(c(birth, death, growth), names_to = "rate") %>%
@@ -38,25 +50,24 @@ tibble(
       aes(N, value, group = rate) +
       geom_line(aes(color = rate)) +
 
+      geom_hline(yintercept = 0) +
+
       geom_vline(
-        aes(xintercept = carrying_capacity)
+        aes(xintercept = carrying_capacity),
+        linetype = "dotted"
       ) +
+
       guides(color = guide_legend(override.aes = list(linewidth=3))) +
       labs(color = NULL, y = "rate", x = "N [count]") +
       theme_bw(14) +
+      theme(aspect.ratio = 1) +
+      theme(legend.position = "bottom") +
       NULL
   }
 #' Let's look at the resulting ODE
 #'
 #'
 u0 <- seq.default(0, 10, by = 1)
-ode_params <- list(
-  beta_0 = 3,
-  mu_0 = 2.9,
-  K_beta = 25,
-  K_mu = 10,
-  alpha_mu = 0.1
-)
 ode_results <- deSolve::ode(
   y = c(u = u0), times = seq.default(0, 10, by = 0.001),
   parms = ode_params,
