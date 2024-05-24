eee286f4ca1cbbcbdb55cd097367dc8e480094f2
Mossa <cgmossa@gmail.com>
2024-05-24 14:36:43 +0200

    wp: changed the propensity thing

--- src/rust/src/simulation.rs
+++ src/rust/src/simulation.rs
@@ -14,33 +14,41 @@ fn sim_bdm(n0: &[i32], birth_baseline: &[f64], death_baseline: &[f64], t_max: f6
     assert_eq!(n0.len(), birth_baseline.len());
     assert_eq!(birth_baseline.len(), death_baseline.len());
     let mut rng = SmallRng::seed_from_u64(20240506);
-    let mut t = 0.0;
     let n0: Vec<u32> = n0.iter().map(|&x| x.try_into().unwrap()).collect_vec();
+
+    // initialize state
     let mut n: Vec<u32> = Vec::with_capacity(n0.len());
-    // n.fill(0);
     n.extend_from_slice(n0.as_slice());
     assert_eq!(n.len(), n0.len());
 
+    let mut t = 0.0;
     loop {
         let delta_t: f64 = rng.sample(rand::distributions::Open01);
-        //TODO: calculate birth and death rates according to the density... 
-        let total_propensity: f64 = izip!(
-            n.iter(),
-            birth_baseline.iter(),
-            death_baseline.iter()
-        )
-        .map(|(&n, b, d)| (n as f64) * (b + d))
-        .sum1()
-        .unwrap();
+        //TODO: calculate birth and death rates according to the density...
+
+        let propensity_birth =
+            izip!(birth_baseline.iter(), n.iter()).map(|(&rate, &n)| rate * (n as f64));
+        let propensity_death =
+            izip!(death_baseline.iter(), n.iter()).map(|(&rate, &n)| rate * (n as f64));
+        let propensity = propensity_birth.chain(propensity_death).collect_vec();
+
+        let total_propensity: f64 = propensity.iter().sum1().unwrap();
         let delta_t = -delta_t.ln() / total_propensity;
         t += delta_t;
 
         if t >= t_max {
             break;
         }
-        
-        // 
 
+        // next event
+        let p: f64 = rng.sample(rand::distributions::OpenClosed01);
+        let p = p * total_propensity;
+        let event = propensity.iter().find_position(|&&x| x >= p);
+        match event {
+            Some((0, _)) => todo!(),
+            Some(_) => todo!(),
+            None => todo!(),
+        }
     }
 
     // birth_baseline.iter().sum::<f64>() +
