e18ab9f4b833ff4d3b272cb17bcdb61c65855afa
Mossa <cgmossa@gmail.com>
2024-06-18 21:31:00 +0200

    wp: okay this also works..

--- notebooks/028_linear_index_to_lower_trig.R
+++ notebooks/028_linear_index_to_lower_trig.R
@@ -9,14 +9,14 @@
 get_ij <- function(n) {
   k <- seq_len(n * (n - 1) / 2) - 1
 
-
   # APPROACH:
   # SOURCE: https://stackoverflow.com/a/27088560/63696
   i <- n - 2 - floor(sqrt(-8 * k + 4 * n * (n - 1) - 7) / 2.0 - 0.5)
-  j <- k + i + 1 - n * (n - 1) / 2 + (n - i) * ((n - i) - 1) / 2
-  
-  cbind(i = i, j = j)
+  # j <- k + i + 1 - n * (n - 1) / 2 + (n - i) * ((n - i) - 1) / 2
+  j <- k + i * (i + 3) / 2 - n * i + 1
 
+  cbind(i = i, j = j)
+  
   # APPROACH: doesn't work
   # kk <- n * (n - 1) / 2 - k
   # p <- floor(
