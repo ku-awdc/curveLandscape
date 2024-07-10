cb5e4add9b3af3d87320044539b66fda8df4c40d
Mossa <cgmossa@gmail.com>
2024-07-10 16:09:09 +0200

    it works now source: Maya

--- notebooks/032_ode_migration.R
+++ notebooks/032_ode_migration.R
@@ -11,19 +11,7 @@ create_ode_model <- function(
     carrying_capacity,
     migration_baseline,
     kernel_distance_flat) {
-  # TODO: construct these
-
-  #
-  # - [x] update_migration_only(population, migration_baseline, carrying_capacity,
-  #   k_dij)
-  # - [ ] update_birth_death_and_migration(
-  #   population, birth_rate, death_Rate, migration_baseline, carrying_capacity,
-  #   k_dij
-  # )
-  # - ?
-
-
-
+  
   ode(
     y = initial_population,
     times = times,
@@ -51,21 +39,105 @@ create_ode_model <- function(
 #'
 devtools::load_all()
 create_ode_model(
-  initial_population = c(0, 50),
-  times = seq.default(0, 300, length.out = 1000),
+  initial_population = c(100, 2),
+  times = seq.default(0, 500, length.out = 1000),
   birth_rate = NA,
   death_rate = NA,
   # carrying capacity contains differences in cell areas
   # and availability of resources
-  carrying_capacity <- c(20,  20),
+  carrying_capacity <- c(20, 20),
   # use migration-baseline for duration of movement season
   # i.e. difference between male vs. female dispersal
   # migration_baseline = c(1, 1) / (6 * 7) * 10,
-  migration_baseline = c(1, 1) * 1e7,
-  kernel_distance_flat = c(1, 1)
+  migration_baseline = c(1, 1) / 10,
+  kernel_distance_flat = c(1)
 ) -> ode_mig_output
 
 ode_mig_output %>%
+  as_tibble() %>%
+  pivot_longer(-time) %>%
+  mutate(carrying_capacity = carrying_capacity[as.integer(name)]) %>%
+  identity() %>%
+  # filter(time < 2) %>% 
+  {
+    ggplot(.) +
+      aes(time) +
+      geom_step(aes(y = value, color = name)) +
+      geom_hline(
+        aes(yintercept = carrying_capacity, color = name),
+        linewidth = 0.9,
+        linetype = "dashed"
+      ) +
+      theme_bw() +
+      theme(legend.position = "bottom") +
+      NULL
+  }
+
+
+#' Birth + Death + Migration
+#'
+#'
+#' #' When developing this, in a terminal run `just watch`.
+devtools::load_all()
+library(deSolve)
+
+#'
+#'
+create_ode_model_bdm <- function(
+    initial_population, times,
+    birth_rate = NULL,
+    death_rate = NULL,
+    carrying_capacity,
+    migration_baseline,
+    kernel_distance_flat) {
+  ode(
+    y = initial_population,
+    times = times,
+    parms = list(
+      birth_rate = birth_rate,
+      death_rate = death_rate,
+      carrying_capacity = carrying_capacity,
+      migration_baseline = migration_baseline,
+      k_dij = kernel_distance_flat
+    ),
+    func = function(t, y, parms, ...) {
+      with(parms, {
+        # function(population_total, birth, death, migration_baseline, carrying_capacity, k_dij) invisible(.Call(wrap__update_birth_death_and_migration, population_total, birth, death, migration_baseline, carrying_capacity, k_dij))
+        update_birth_death_and_migration(
+          population_total = y,
+          birth = birth_rate,
+          death = death_rate,
+          migration_baseline, carrying_capacity,
+          k_dij
+        )
+
+        list(
+          c(dN = y)
+        )
+      })
+    }
+  )
+}
+#'
+#'
+devtools::load_all()
+create_ode_model_bdm(
+  initial_population = c(1, 5),
+  times = seq.default(0, 10 * 365, length.out = 1000),
+  birth_rate = 4 / 365,
+  death_rate = 1 / 365,
+  # carrying capacity contains differences in cell areas
+  # and availability of resources
+  carrying_capacity <- c(20, 20),
+  # use migration-baseline for duration of movement season
+  # i.e. difference between male vs. female dispersal
+  # migration_baseline = c(1, 1) / (6 * 7) * 10,
+  # migration_baseline = c(1, 1) / (6*7),
+  migration_baseline = c(0, 0),
+  kernel_distance_flat = c(1)
+) -> ode_bdm_output
+
+ode_bdm_output %>%
   as_tibble() %>%
   pivot_longer(-time) %>%
   mutate(carrying_capacity = carrying_capacity[as.integer(name)]) %>%
--- src/rust/src/simulation.rs
+++ src/rust/src/simulation.rs
@@ -317,28 +317,38 @@ fn update_migration_only(
     k_dij: &[f64],
 ) {
     let n_len = population_total.len();
+    // k_dij is assumed to be lower triangular, column-wise, w/o diagonal distance "matrix"
+    assert_eq!(k_dij.len(), get_total_number_of_elements(n_len));
+
+    // rprintln!("before migration {:?}", population_total);
     let n_current = population_total.to_owned();
     let n_current: &[f64] = n_current.as_ref();
 
     population_total.fill(0.);
     for k in 0..k_dij.len() {
         let [i, j] = get_row_col(k, n_len);
+        rprintln!("(i,j) = ({},{})", i, j);
         // since k_dij is symmetric, k_dij[k] "=" k_dji[k] let us say...
         // let equation_1_ji = migration_baseline[i] * k_dij[k] * (-carrying_capacity[i]).exp();
         // let equation_1_ij = migration_baseline[j] * k_dij[k] * (-carrying_capacity[j]).exp();
 
         // verified!
-        // let equation_1_ji = migration_baseline[i] * k_dij[k] / carrying_capacity[i];
-        // let equation_1_ij = migration_baseline[j] * k_dij[k] / carrying_capacity[j];
-        let equation_1_ji = migration_baseline[i] * k_dij[k] * (-carrying_capacity[i]).exp();
-        let equation_1_ij = migration_baseline[j] * k_dij[k] * (-carrying_capacity[j]).exp();
+        let equation_1_ji = migration_baseline[i] * k_dij[k] / carrying_capacity[i];
+        let equation_1_ij = migration_baseline[j] * k_dij[k] / carrying_capacity[j];
+        // let equation_1_ji = migration_baseline[i] * k_dij[k] * (-carrying_capacity[i]).exp();
+        // let equation_1_ij = migration_baseline[j] * k_dij[k] * (-carrying_capacity[j]).exp();
 
+        // for debug purpose
+        // let equation_1_ji = migration_baseline[i];
+        // let equation_1_ij = migration_baseline[j];
         let rate_equation_1_ji = equation_1_ji * n_current[i] as f64;
         let rate_equation_1_ij = equation_1_ij * n_current[j] as f64;
-        //TODO: test if they are sign positive?
+
         population_total[i] += rate_equation_1_ij - rate_equation_1_ji;
         population_total[j] += rate_equation_1_ji - rate_equation_1_ij;
     }
+
+    // rprintln!("after migration {:?}", population_total);
 }
 
 /// Updates the population state vector according to the migration equation together with
@@ -361,19 +371,42 @@ fn update_birth_death_and_migration(
     k_dij: &[f64],
 ) {
     let n_len = population_total.len();
+    // k_dij is assumed to be lower triangular, column-wise, w/o diagonal distance "matrix"
+    assert_eq!(k_dij.len(), get_total_number_of_elements(n_len));
 
-    update_migration_only(
-        population_total,
-        migration_baseline,
-        carrying_capacity,
-        k_dij,
-    );
+    let n_current = population_total.to_owned();
+    let n_current: &[f64] = n_current.as_ref();
+
+    population_total.fill(0.);
+    for k in 0..k_dij.len() {
+        let [i, j] = get_row_col(k, n_len);
+        // since k_dij is symmetric, k_dij[k] "=" k_dji[k] let us say...
+        // let equation_1_ji = migration_baseline[i] * k_dij[k] * (-carrying_capacity[i]).exp();
+        // let equation_1_ij = migration_baseline[j] * k_dij[k] * (-carrying_capacity[j]).exp();
+
+        // verified!
+        let equation_1_ji = migration_baseline[i] * k_dij[k] / carrying_capacity[i];
+        let equation_1_ij = migration_baseline[j] * k_dij[k] / carrying_capacity[j];
+        // let equation_1_ji = migration_baseline[i] * k_dij[k] * (-carrying_capacity[i]).exp();
+        // let equation_1_ij = migration_baseline[j] * k_dij[k] * (-carrying_capacity[j]).exp();
+
+        // for debug purpose
+        // let equation_1_ji = migration_baseline[i];
+        // let equation_1_ij = migration_baseline[j];
+        let rate_equation_1_ji = equation_1_ji * n_current[i] as f64;
+        let rate_equation_1_ij = equation_1_ij * n_current[j] as f64;
+
+        population_total[i] += rate_equation_1_ij - rate_equation_1_ji;
+        population_total[j] += rate_equation_1_ji - rate_equation_1_ij;
+    }
 
     // after migration, apply birth/death dynamics.
     let growth_rate: f64 = birth - death;
+    // rprintln!("growth_rate: {growth_rate}");
     for i in 0..n_len {
+        // rprintln!("before {}", population_total[i]);
         population_total[i] +=
-            population_total[i] * growth_rate * (1. - population_total[i] / carrying_capacity[i]);
+            n_current[i] * growth_rate * (1. - n_current[i] / carrying_capacity[i]);
     }
 }
 
