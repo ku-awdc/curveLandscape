dabcbad8bc2e803e3bb79b1f27109485714712ed
Mossa <cgmossa@gmail.com>
2024-08-07 17:09:31 +0200

    added carrying capacity to the plot.. now need to have multiple simulations..

--- notebooks/042_ode_plus_gillespie_two_patch.R
+++ notebooks/042_ode_plus_gillespie_two_patch.R
@@ -30,7 +30,7 @@ ssa_source_only_wedge(
   n0 = as.integer(c(10, 0)),
   birth_baseline = c(4, 4),
   death_baseline = c(1, 1),
-  carrying_capacity = as.integer(c(2, 8)),
+  carrying_capacity <- as.integer(c(2, 8)),
   migration_baseline = 1 / (8 / 12),
   t_max = 5
 ) -> ssa_output
@@ -55,7 +55,9 @@ ode_source_only_wedge(
   t_max = 5
 ) -> deter_output
 
-deter_output
+#' Add carrying capacity information for plotting...
+deter_output <- deter_output %>%
+  mutate(carrying_capacity = carrying_capacity[as.integer(id_patch)])
 
 deter_output %>%
   identity() %>%
@@ -63,25 +65,35 @@ deter_output %>%
     ggplot(.) +
       aes(time, N, group = id_patch) +
       geom_line(
-        # alpha = 0.6,
+        # alpha = 0.5,
         aes(color = factor(id_patch)),
         show.legend = FALSE,
         linetype = "dashed"
       ) +
-      
       geom_step(
-        alpha  = 0.5,
+        alpha = 0.5,
         aes(
           group = str_c(id_patch, repetition),
           color = factor(id_patch)
         ),
-        data = ssa_output
+        data = ssa_output,
+        show.legend = FALSE
       ) +
       labs(
         color = NULL,
         y = "Patch-level count",
         x = "time [years]"
       ) +
+      geom_hline(
+        aes(
+          yintercept = carrying_capacity,
+          # color = id_patch
+        ),
+        color = "forestgreen",
+        linetype = "dotdash",
+        linewidth = 0.3
+      ) +
+
       # scale_colour_manual(
       #   values = c(
       #     "ODE" = "black",
@@ -90,6 +102,8 @@ deter_output %>%
       # ) +
       # expand_limits(y = c(0, 2, 8)) +
       theme_bw(base_size = 15) +
+      # ggpubr::theme_transparent() +
       theme(legend.position = "bottom") +
       NULL
   }
+
