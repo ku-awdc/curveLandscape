dc9b7b4fdc84738d93bf4e03f357e0d18bbdea78
Mossa <cgmossa@gmail.com>
2024-08-07 17:26:33 +0200

    Added plots..

--- notebooks/042_ode_plus_gillespie_two_patch.R
+++ notebooks/042_ode_plus_gillespie_two_patch.R
@@ -10,18 +10,23 @@ ssa_source_only_wedge <- function(
     death_baseline,
     carrying_capacity,
     migration_baseline,
-    t_max) {
-  sim_bdm(
-    n0 = n0,
-    birth_baseline = birth_baseline,
-    death_baseline = death_baseline,
-    carrying_capacity = carrying_capacity,
-    migration_baseline = migration_baseline,
-    t_max = t_max
-  ) -> sim_output
-
-  sim_output %>%
-    as_tibble() %>%
+    t_max,
+    reps = 100) {
+  map(
+    seq_len(reps), \(rep) {
+      sim_bdm(
+        n0 = n0,
+        birth_baseline = birth_baseline,
+        death_baseline = death_baseline,
+        carrying_capacity = carrying_capacity,
+        migration_baseline = migration_baseline,
+        t_max = t_max
+      ) %>%
+        as_tibble() %>%
+        mutate(repetition = rep)
+    }
+  ) %>%
+    bind_rows() %>%
     rename(N = count) %>%
     mutate(id_patch = id_patch + 1)
 }
@@ -32,7 +37,8 @@ ssa_source_only_wedge(
   death_baseline = c(1, 1),
   carrying_capacity <- as.integer(c(2, 8)),
   migration_baseline = 1 / (8 / 12),
-  t_max = 5
+  t_max = 5,
+  reps = 100
 ) -> ssa_output
 
 ssa_output
@@ -70,8 +76,8 @@ deter_output %>%
         show.legend = FALSE,
         linetype = "dashed"
       ) +
-      geom_step(
-        alpha = 0.5,
+      geom_line(
+        alpha = 0.1,
         aes(
           group = str_c(id_patch, repetition),
           color = factor(id_patch)
@@ -79,6 +85,14 @@ deter_output %>%
         data = ssa_output,
         show.legend = FALSE
       ) +
+      # stat_smooth(
+      #   data = ssa_output,
+      #   aes(
+      #     color = factor(id_patch)
+      #   ),
+      #   linewidth = .8,
+      #   se = FALSE
+      # ) +
       labs(
         color = NULL,
         y = "Patch-level count",
@@ -107,3 +121,48 @@ deter_output %>%
       NULL
   }
 
+
+
+#' Filter the data as to only be `t_max = 1`
+
+deter_output %>%
+  filter(time <= 1) %>%
+  identity() %>%
+  {
+    ggplot(.) +
+      aes(time, N, group = id_patch) +
+      geom_line(
+        # alpha = 0.5,
+        aes(color = factor(id_patch)),
+        show.legend = FALSE,
+        linetype = "dashed"
+      ) +
+      geom_line(
+        alpha = 0.1,
+        aes(
+          group = str_c(id_patch, repetition),
+          color = factor(id_patch)
+        ),
+        data = ssa_output %>%
+          filter(time <= 1),
+        show.legend = FALSE
+      ) +
+      labs(
+        color = NULL,
+        y = "Patch-level count",
+        x = "time [years]"
+      ) +
+      geom_hline(
+        aes(
+          yintercept = carrying_capacity,
+          # color = id_patch
+        ),
+        color = "forestgreen",
+        linetype = "dotdash",
+        linewidth = 0.3
+      ) +
+      expand_limits(y = c(0, 2, 8)) +
+      theme_bw(base_size = 15) +
+      theme(legend.position = "bottom") +
+      NULL
+  }
