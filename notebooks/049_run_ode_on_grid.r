fdd1c4ff27eb498c9128bb5c50e2dca8f21b2aa0
Mossa <cgmossa@gmail.com>
2024-08-09 17:26:38 +0200

    finished: runs the ODE on grids

--- notebooks/049_run_ode_on_grid.r
+++ notebooks/049_run_ode_on_grid.r
@@ -4,39 +4,59 @@ all_landscapes
 
 naive_grid <- all_landscapes$grid[[5]]
 
-ode_source_only_wedge(
-  # n0 = ceiling(naive_grid$cc) %>% as.integer(),
-  n0 = pmin(naive_grid$Capacity, 1),
-  # birth_baseline = rep.int(4., nrow(naive_grid)),
-  # death_baseline = rep.int(1., nrow(naive_grid)),
-  growth_rate = 4 - 1,
-  carrying_capacity = naive_grid$Capacity,
-  migration_baseline = 1 / (8 / 12),
-  delta_t = 1/52,
-  t_max = 5,
-  # reps = 100
-) -> ode_output
+all_landscapes_trans <- transpose(all_landscapes)
 
-ode_output %>%
-  glimpse() %>% 
-  identity() %>% {
-    ggplot(.) +
-      aes(time, N, group = id_patch) +
-      geom_step(aes(alpha = N), show.legend = FALSE) +
-      ggpubr::theme_pubclean(15) +
-      # theme_light(15) +
-      NULL
-  }
 
-  ode_output %>%
-    glimpse() %>% 
-    filter(time<=2) %>% 
-    identity() %>% {
-      ggplot(.) +
-        aes(time, N, group = id_patch) +
-        geom_step(aes(alpha = N), show.legend = FALSE) +
-        labs(x = "time [years]") +
-        ggpubr::theme_pubclean(15) +
-        # theme_light(15) +
-        NULL
-    }
+pdf("figures/049_run_ode_on_all_landscapes.pdf")
+
+for (id_grid in seq_along(all_landscapes_trans)) {
+  with(all_landscapes_trans[[id_grid]], {
+    ode_source_only_wedge(
+      # n0 = ceiling(grid$cc) %>% as.integer(),
+      n0 = pmin(grid$Capacity, 1),
+      # birth_baseline = rep.int(4., nrow(grid)),
+      # death_baseline = rep.int(1., nrow(grid)),
+      growth_rate = 4 - 1,
+      carrying_capacity = grid$Capacity,
+      migration_baseline = 1 / (8 / 12),
+      delta_t = 1 / 52,
+      t_max = 5,
+      # reps = 100
+    ) -> ode_output
+
+    ode_output %>%
+      glimpse() %>%
+      identity() %>%
+      {
+        ggplot(.) +
+          aes(time, N, group = id_patch) +
+          geom_step(aes(alpha = N), show.legend = FALSE) +
+          labs(x = "time [years]") +
+          labs(caption = glue("Landscape type {landscape_type}, and total patches {n_len}")) +
+          ggpubr::theme_pubclean(15) +
+          # theme_light(15) +
+          NULL
+      } -> p_ode
+
+    print(p_ode)
+
+    ode_output %>%
+      glimpse() %>%
+      filter(time <= 2) %>%
+      identity() %>%
+      {
+        ggplot(.) +
+          aes(time, N, group = id_patch) +
+          geom_step(aes(alpha = N), show.legend = FALSE) +
+          labs(x = "time [years]") +
+          labs(caption = glue("Landscape type {landscape_type}, and total patches {n_len}")) +
+          ggpubr::theme_pubclean(15) +
+          # theme_light(15) +
+          NULL
+      } -> p_ode_zoom
+
+    print(p_ode_zoom)
+  })
+}
+
+dev.off()
