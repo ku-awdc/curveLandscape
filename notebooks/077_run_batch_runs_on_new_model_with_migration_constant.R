a6e8b87a9b336373bf7e8a05a3e995c3a9432382
Mossa <cgmossa@gmail.com>
2024-08-21 21:57:48 +0200

    added batch mode for constant..

---
+++ notebooks/077_run_batch_runs_on_new_model_with_migration_constant.R
@@ -0,0 +1,225 @@
+devtools::load_all()
+
+
+all_landscapes
+
+#' Pre-allocate slots for the binned_output, the stat_output, and ignore error for now...
+all_landscapes$binned_output <- vector("list", length = nrow(all_landscapes))
+all_landscapes$stat_binned_output <- vector("list", length = nrow(all_landscapes))
+
+# migration_baseline <- 1 / (8 / 12)
+# repetitions <- 250
+# repetitions <- 50
+repetitions <- 50
+t_max <- 25
+delta_t <- 1 / 12
+all_migration_baseline <- 1 / (8 / 12)
+# all_migration_baseline <- 0
+binned_time <- seq.default(from = 0, to = t_max, by = delta_t)
+
+fs::dir_create("figures")
+pdf("figures/077_plot_new_model_smooth_with_migration_constant.pdf")
+for (id_m0 in seq_along(all_migration_baseline)) {
+  migration_baseline <- all_migration_baseline[id_m0]
+
+  # Amend simulation details to the object
+  all_landscapes <- structure(all_landscapes,
+    simulation_configuration = list(
+      migration_baseline = migration_baseline,
+      migration_intercept = 0,
+      repetitions = repetitions,
+      t_max = t_max,
+      delta_t = delta_t
+    )
+  )
+
+  all_landscapes_trans <- purrr::transpose(all_landscapes)
+  for (id_landscape in seq_along(all_landscapes_trans)) {
+    landscape <- all_landscapes_trans[[id_landscape]]
+    print("processing")
+    print(landscape$set_patch_size)
+
+    # Initialise at ideal equilibrium
+    n0 <- landscape$grid$Capacity %>%
+      round() %>%
+      as.integer()
+
+    p_landscape_caption <-
+      labs(caption = glue("Landscape type {landscape$landscape_type}, with patch-size {landscape$set_patch_size} km^2, and total patches {landscape$n_len}."))
+
+    wild_ssa_model <- WildSSA$new(
+      n0 = n0,
+      birth_baseline = rep.int(4, times = landscape$n_len),
+      death_baseline = rep.int(1, times = landscape$n_len),
+      migration_intercept = 0,
+      carrying_capacity = landscape$grid$Capacity,
+      migration_baseline = migration_baseline
+    )
+
+    wild_ssa_model$run_and_record_fixed_time_population_par(
+      t_max = t_max,
+      fixed_time_points = binned_time,
+      repetitions = repetitions,
+      20002224
+    ) -> wild_ssa_output
+
+    df_binned_ssa_output <- wild_ssa_output %>%
+      bind_rows() %>%
+      rename(population_count = count) %>%
+      select(-current_time, -current_count, -current_time_index) %>%
+      mutate(id_time = seq_len(n()), .by = repetition)
+
+
+    all_landscapes$binned_output[[id_landscape]] <- df_binned_ssa_output
+    #' ## Binned pr repetition population count plot..
+
+    df_binned_ssa_output %>%
+      glimpse() %>%
+      {
+        ggplot(.) +
+          aes(time, population_count, group = repetition) +
+          geom_step(aes(alpha = population_count, color = repetition), show.legend = FALSE) +
+          labs(x = "time [years]") +
+          geom_hline(
+            aes(yintercept = landscape$total_cc),
+            linetype = "dotted",
+            linewidth = 1.1
+          ) +
+          labs(y = "Population count by replicate") +
+          p_landscape_caption +
+          ggpubr::theme_pubclean(15) +
+          NULL
+      } -> p_binned_population_count
+    print(p_binned_population_count)
+
+    # Binned and statistical bandwidth of each signal..
+
+    stat_wild_ssa_output <-
+      df_binned_ssa_output %>%
+      group_by(id_time) %>%
+      reframe(
+        time = time[1],
+        mean = mean(population_count),
+        ci_lower = quantile(population_count, probs = 0.025),
+        ci_upper = quantile(population_count, probs = 1 - 0.025)
+      ) %>%
+      select(-id_time)
+    stat_wild_ssa_output
+    all_landscapes$stat_binned_output[[id_landscape]] <- stat_wild_ssa_output
+
+    stat_wild_ssa_output %>%
+      glimpse() %>%
+      {
+        ggplot(.) +
+          geom_line(aes(time, mean)) +
+          geom_line(linetype = "dotted", aes(time, ci_lower)) +
+          geom_line(linetype = "dotdash", aes(time, ci_upper)) +
+          # geom_step(aes(alpha = count, color = repetition), show.legend = FALSE) +
+          labs(x = "time [years]") +
+          labs(y = "Mean population count") +
+          geom_hline(
+            aes(yintercept = landscape$total_cc),
+            linetype = "dotted",
+            linewidth = 1.1
+          ) +
+          p_landscape_caption +
+          ggpubr::theme_pubclean(15) +
+          # theme_light(15) +
+          NULL
+      } -> p_binned_stat_ssa_output
+    # print(p_binned_stat_ssa_output)
+
+    #' ## Full resolution
+    #'
+    #'
+    if (FALSE) {
+      total_cc
+
+      wild_ssa_output <- wild_ssa_output %>%
+        bind_rows()
+      wild_ssa_output %>%
+        glimpse() %>%
+        identity() %>%
+        {
+          ggplot(.) +
+            aes(time, count, group = repetition) +
+            geom_step(aes(alpha = count, color = repetition), show.legend = FALSE) +
+            labs(x = "time [years]") +
+            geom_hline(
+              aes(yintercept = total_cc),
+              linetype = "dotted",
+              linewidth = 1.1
+            ) +
+            p_landscape_caption +
+            ggpubr::theme_pubclean(15) +
+            # theme_light(15) +
+            NULL
+        }
+    }
+  }
+
+  # write_rds(all_landscapes, ".cache/056_all_landscapes.rds")
+  # all_landscapes <- read_rds(".cache/056_all_landscapes.rds")
+
+
+  #' These landscapes were simulated with:
+  attributes(all_landscapes)$simulation_configuration
+
+  all_landscapes %>%
+    mutate(
+      landscape_type = fct_inorder(landscape_type),
+      set_patch_size_label = fct(set_patch_size %>% str_c(" km²"))
+    ) %>%
+    select(landscape_type:n_len, stat_binned_output, set_patch_size_label, -grid) %>%
+    unnest(stat_binned_output) %>%
+    identity() %>%
+    glimpse() %>%
+    {
+      ggplot(.) +
+        aes(time, mean, group = str_c(landscape_type, set_patch_size_label)) +
+        geom_line(
+          aes(color = set_patch_size_label),
+        ) +
+        geom_hline(
+          aes(yintercept = all_landscapes$total_cc[[1]]),
+          linetype = "dotted",
+          linewidth = 1.1
+        ) +
+        labs(x = "time [years]") +
+        labs(y = "Mean Total Population over replicates") +
+        labs(caption = glue_data(
+          attributes(all_landscapes)$simulation_configuration,
+          "Migration baseline {migration_baseline}; Replicates: {repetitions}"
+        )) +
+        scale_color_viridis_d(direction = 1) +
+        # confidence interval... but they are very low..
+        geom_line(linetype = "dotdash", aes(time, ci_lower, color = set_patch_size_label)) +
+        geom_line(linetype = "dotdash", aes(time, ci_upper, color = set_patch_size_label)) +
+        labs(color = NULL) +
+        guides(color = guide_legend(override.aes = list(linewidth = 1.2))) +
+        facet_grid(
+          rows = vars(landscape_type),
+          labeller = labeller(landscape_type = c(naive = "grid-based", habic = "habitat-based"))
+        ) +
+        ggpubr::theme_pubclean(15) +
+        NULL
+    } -> p_plot_all_landscapes_together
+
+  print(p_plot_all_landscapes_together)
+  write_rds(all_landscapes, glue(
+    ".cache/077_new_model_all_landscapes_default_m0_constant.rds"
+  ))
+  # fs::dir_create("figures")
+  ggsave(
+    p_plot_all_landscapes_together,
+    filename = "figures/077_new_model_all_landscapes_default_m0_constant.svg",
+    device = svglite::svglite,
+    # scaling = 3,
+    # scale = 1.9
+    # width = 2*4.27,
+    # height = 2.5
+  )
+}
+
+beepr::beep("complete")
+dev.off()
