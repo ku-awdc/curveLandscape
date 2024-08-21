f59ab999c6b53a670f60c0fc3e55bf3c2b8634bc
Mossa <cgmossa@gmail.com>
2024-08-21 09:01:27 +0200

    TODO: add this comparison test now that the recorder is filling in...

---
+++ tests/testthat/test-fixed_population_recorder.R
@@ -0,0 +1,8 @@
+test_that("equal to R's approx", {
+
+  # R's approx(method = "constant", f = 0)
+  # must agree with the `FixedPopulationRecorder` output in `WildSSA`
+
+  # MISSING
+  expect_false(true) 
+})
