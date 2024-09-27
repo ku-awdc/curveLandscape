1286ebdc596c11e64753432abd92be3e11055051
Matt Denwood <md@sund.ku.dk>
2024-09-27 08:05:24 +0200

    Implement utility function for comparing models at patch level

---
+++ R/all_models_patch_level.R
@@ -0,0 +1,135 @@
+#' Get results for all 4 models (ODE and SDE) at patch level
+#'
+#' @param carrying_capacity vector of carrying capacity
+#' @param n0 vector of starting numbers
+#'
+#' @export
+all_models_patch_level <- function(carrying_capacity, n0=carrying_capacity, t_max=5, repetitions = 250){
+
+  stopifnot(length(carrying_capacity)==length(n0))
+
+  n0 <- as.integer(n0)
+  growth_rate <- 4-1
+  birth_baseline <- rep.int(4, times = length(n0))
+  death_baseline <- rep.int(1, times = length(n0))
+  migration_offset <- 0
+  migration_baseline <- 1 / (8 / 12)
+  migration_intercept <- 0
+  delta_t <- 1 / (12 * 4)
+  fixed_times <- seq.default(0, t_max, by = delta_t)
+
+
+  ## ODEs
+  bind_rows(
+    ode_migration_only(
+      growth_rate = growth_rate,
+      carrying_capacity = carrying_capacity,
+      n0 = n0,
+      migration_offset = 0,
+      migration_baseline = 0,
+      migration_intercept = 0,
+      delta_t = delta_t,
+      t_max = t_max
+    ) |> mutate(Model = "NoMigration"),
+    ode_migration_only(
+      growth_rate = growth_rate,
+      carrying_capacity = carrying_capacity,
+      n0 = n0,
+      migration_offset = migration_offset,
+      migration_baseline = migration_baseline,
+      migration_intercept = migration_intercept,
+      delta_t = delta_t,
+      t_max = t_max
+    ) |> mutate(Model = "Static"),
+    ode_source_only_wedge(
+      growth_rate = growth_rate,
+      carrying_capacity = carrying_capacity,
+      n0 = n0,
+      migration_offset = migration_offset,
+      migration_baseline = migration_baseline,
+      migration_intercept = migration_intercept,
+      delta_t = delta_t,
+      t_max = t_max
+    ) |> mutate(Model = "Wedge"),
+    ode_source_only_smooth(
+      growth_rate = growth_rate,
+      carrying_capacity = carrying_capacity,
+      n0 = n0,
+      migration_offset = migration_offset,
+      migration_baseline = migration_baseline,
+      migration_intercept = migration_intercept,
+      delta_t = delta_t,
+      t_max = t_max
+    ) |> mutate(Model = "Smooth"),
+  ) |>
+  select(Time = time, Patch = id_patch, N, Model) |>
+  mutate(Type = "ODE", Iteration=0) ->
+  ode_results
+
+#  yints <- tibble(CC = carrying_capacity) |> mutate(Patch = row_number())
+#  ggplot(ode_results, aes(x=Time, y=N, col=Model)) +
+#    geom_line() +
+#    facet_wrap(~Patch) +
+#    geom_hline(aes(yintercept=CC), yints, lty="dashed")
+
+  ## SDEs
+  models=result <- list(
+    nomig = NA,
+    static = NA,
+    wedge = NA,
+    smooth = NA
+  )
+
+  models$nomig <-
+    WildSSA$new_static(n0, birth_baseline, death_baseline, carrying_capacity, 0.0, 0.0)
+  models$static <-
+    WildSSA$new_static(n0, birth_baseline, death_baseline, carrying_capacity, migration_intercept, migration_baseline)
+  models$wedge <-
+    WildSSA$new_wedge(n0, birth_baseline, death_baseline, carrying_capacity, migration_intercept, migration_baseline)
+  models$smooth <-
+    WildSSA$new_smooth(n0, birth_baseline, death_baseline, carrying_capacity, migration_intercept, migration_baseline)
+
+  result$nomig <- models$nomig$run_and_record_fixed_time_patches_par(fixed_time_points = fixed_times, t_max, repetitions, seed)
+  result$static <- models$static$run_and_record_fixed_time_patches_par(fixed_time_points = fixed_times, t_max, repetitions, seed)
+  result$wedge <- models$wedge$run_and_record_fixed_time_patches_par(fixed_time_points = fixed_times, t_max, repetitions, seed)
+  result$smooth <- models$smooth$run_and_record_fixed_time_patches_par(fixed_time_points = fixed_times, t_max, repetitions, seed)
+
+  all_patch_result_df <- result %>%
+    enframe("migration_scheme", "output") %>%
+    unnest_longer(output) %>%
+    unnest_wider(output) %>%
+    select(-current_time, -current_count, -current_time_index, -fixed_time) %>%
+    rename(patch_count = count) %>%
+    # mutate(id_time = list(seq_along(time[[1]]))) %>%
+    # unnest(c(id_time, time, id_patch, patch_count)) %>%
+    unnest(c(time, id_patch, patch_count)) %>%
+    mutate(id_patch = id_patch + 1) %>%
+    # mutate(id_time = rep())
+    identity()
+
+  all_patch_result_df |>
+    mutate(Iteration = repetition+1) |>
+    mutate(Model = case_match(migration_scheme,
+      "nomig" ~ "NoMigration",
+      "static" ~ "Static",
+      "wedge" ~ "Wedge",
+      "smooth" ~ "Smooth"
+    ), Type = "SDE") |>
+    select(Time = time, Patch = id_patch, N = patch_count, Model, Iteration, Type) ->
+    sde_results
+
+  bind_rows(
+    ode_results |> mutate(Patch = as.integer(Patch)),
+    sde_results,
+    sde_results |> group_by(Time, Model, Patch, Type) |> summarise(N = mean(N), Iteration=0, .groups="drop")
+  ) ->
+    all_res
+
+  bind_rows(
+    all_res,
+    all_res |>
+      group_by(Time, Model, Type, Iteration) |>
+      mutate(N = sum(N), Patch=0) |>
+      ungroup()
+  )
+}
--- notebooks/082_three_pop_models.R
+++ notebooks/082_three_pop_models.R
@@ -2,7 +2,106 @@ devtools::load_all()
 
 library(tidyverse)
 
-all_landscapes
+## 3-patch scenario:
+### patches have 0.75 a/km2, 0.25 a/km2, 0.1 a/km2
+dens <- c(0.75, 0.5, 0.25)
+### Sizes are 1,2,5,10,25
+sizes <- c(1,2,5,10,25)
+lapply(sizes, \(x) x*dens)
+
+#ccs <- c(19.9,7.9,9.9)
+#n0s <- c(10,0,0)
+
+ccs <- dens*sizes[2]
+ccs <- c(10,0.5,0.5)
+n0s <- c(sum(ccs),1,1)
+
+res <- all_models_patch_level(ccs, n0s)
+ggplot(res |> filter(Iteration==0), aes(x=Time, y=N, col=Type)) +
+  geom_line() +
+  facet_grid(Patch~Model, scales="free_y")
+
+ode_migration_only(
+  growth_rate = 4 - 1,
+  carrying_capacity = ccs,
+  n0 = n0s,
+  migration_offset = 0,
+  migration_baseline = 1 / (8 / 12),
+  migration_intercept = 0,
+  delta_t = 1 / (12 * 4),
+  t_max = 5
+)
+
+ode_source_only_smooth(
+  growth_rate = 4 - 1,
+  carrying_capacity = ccs,
+  n0 = n0s,
+  migration_offset = 0,
+  migration_baseline = 1 / (8 / 12),
+  migration_intercept = 0,
+  delta_t = 1 / (12 * 4),
+  t_max = 5
+) -> deter_output
+
+#' Add carrying capacity information for plotting...
+deter_output <- deter_output %>%
+  mutate(carrying_capacity = carrying_capacity[as.integer(id_patch)])
+
+deter_output %>%
+  identity() %>%
+  {
+    ggplot(.) +
+      aes(time, N, group = id_patch) +
+      geom_line(
+        # alpha = 0.5,
+        aes(color = factor(id_patch)),
+        show.legend = FALSE,
+        # linetype = "dashed"
+      ) +
+      # geom_line(
+      #   data = ssa_output,
+      #   alpha = 0.1,
+      #   aes(
+      #     group = str_c(id_patch, repetition),
+      #     color = factor(id_patch)
+      #   ),
+      #   show.legend = FALSE
+      # ) +
+      # stat_smooth(
+      #   data = ssa_output,
+      #   aes(
+      #     color = factor(id_patch)
+      #   ),
+      #   linewidth = .8,
+      #   se = FALSE
+      # ) +
+      labs(
+        color = NULL,
+        y = "Patch-level count",
+        x = "time [years]"
+      ) +
+      geom_hline(
+          yintercept = ccs,
+        color = "forestgreen",
+        linetype = "dotdash",
+        # linewidth = 0.3
+      ) +
+
+      # scale_colour_manual(
+      #   values = c(
+      #     "ODE" = "black",
+      #     "Stochastic" = "lightblue"
+      #   )
+      # ) +
+      # expand_limits(y = c(0, 2, 8)) +
+      #ylim(c(NA, 10)) +
+      theme_bw(base_size = 15) +
+      # ggpubr::theme_transparent() +
+      theme(legend.position = "bottom") +
+      NULL
+  }
+
+
 
 
 result <- list(
@@ -11,10 +110,10 @@ result <- list(
   smooth = NA
 )
 
-n0 <- as.integer(c(1, 2, 3))
+n0 <- n0s |> as.integer()
 birth_baseline <- rep.int(4, times = length(n0))
 death_baseline <- rep.int(1, times = length(n0))
-carrying_capacity <- c(5, 2, 20)
+carrying_capacity <- ccs
 migration_intercept <- 0
 migration_baseline <- 1 / (8 / 12)
 
@@ -30,8 +129,11 @@ stopifnot(length(n0) == length(carrying_capacity))
 
 models <- result
 
+models$nomig <-
+  WildSSA$new_static(n0, birth_baseline, death_baseline, carrying_capacity, 0.0, 0.0)
+
 models$static <-
-  WildSSA$new_static(n0, birth_baseline, death_baseline, carrying_capacity, migration_intercept, migration_baseline)
+  WildSSA$new_static(n0, birth_baseline, death_baseline, carrying_capacity, 0.0, 0.0)
 
 models$wedge <-
   WildSSA$new_wedge(n0, birth_baseline, death_baseline, carrying_capacity, migration_intercept, migration_baseline)
@@ -55,6 +157,7 @@ all_result_df <-
   mutate(id_time = list(seq_along(fixed_times))) %>%
   unnest(c(id_time, time, population_count))
 
+if(FALSE){
 all_result_df %>%
   glimpse() %>%
   mutate(migration_scheme = fct_inorder(migration_scheme)) %>%
@@ -72,7 +175,7 @@ all_result_df %>%
       ggpubr::theme_pubclean(15) +
       NULL
   }
-
+}
 
 all_result_df %>%
   glimpse() %>%
@@ -150,9 +253,18 @@ all_patch_result_df <- patch_result %>%
 # repetitions
 # 3 * 301 * 250 * 3
 
+all_patch_result_df |>
+  group_by(repetition, time, migration_scheme) |>
+  mutate(total = sum(patch_count)) |>
+  ungroup() |>
+  summary()
+
 
 all_patch_result_df %>%
-  glimpse() %>%
+  group_by(repetition, time, migration_scheme) |>
+  mutate(total = sum(patch_count)) |>
+  #filter(total > 0) |>
+  ungroup() |>
   reframe(
     .by = c(migration_scheme, id_patch, time),
     time = time[1],
